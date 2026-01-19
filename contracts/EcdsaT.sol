87ec3fde64b6904dc42a722fb9b583fe97e90513
bobxwang <sevenz_da_best@hotmail.com>
2026-01-19 13:43:00 +0800

    function selection

--- contracts/EcdsaT.sol
+++ contracts/EcdsaT.sol
@@ -11,4 +11,15 @@ contract EcdsaT {
     function toEthSignedMessageHash(bytes32 hash) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
     }
+
+    // 手动计算函数选择器
+    function getTransferSelector() public pure returns (bytes4) {
+        return bytes4(keccak256("transfer(address,uint256)"));
+        // 返回: 0xa9059cbb
+    }
+
+    function getSetSelector() public pure returns (bytes4) {
+        return bytes4(keccak256("set(uint256)"));
+        // 返回: 0x60fe47b1
+    }
 }

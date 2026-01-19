b95078ca40305d25b88d63dc7efc52040c3c6851
bobxwang <sevenz_da_best@hotmail.com>
2026-01-19 13:52:27 +0800

    ecdsa

--- contracts/EcdsaT.sol
+++ contracts/EcdsaT.sol
@@ -2,7 +2,13 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
 contract EcdsaT {
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+
     function getMessageHash(address account_, uint256 tokenId_) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(account_, tokenId_));
     }
@@ -21,5 +27,22 @@ contract EcdsaT {
     function getSetSelector() public pure returns (bytes4) {
         return bytes4(keccak256("set(uint256)"));
         // 返回: 0x60fe47b1
+    }
+
+    function verify(
+        address signer,
+        string memory message,
+        bytes memory signature
+    ) public pure returns (bool) {
+        // 1. 对消息进行哈希
+        bytes32 messageHash = keccak256(abi.encodePacked(message));
+
+        // 2. 添加以太坊签名前缀
+        bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
+
+        // 3. 恢复签名者地址并验证
+        address recoveredSigner = ethSignedMessageHash.recover(signature);
+
+        return recoveredSigner == signer;
     }
 }

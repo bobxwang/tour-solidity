996bfc0a208745495bb58648a69856b65e31a216
bobxwang <sevenz_da_best@hotmail.com>
2026-01-21 09:53:32 +0800

    Update CatNFT.sol

--- contracts/CatNFT.sol
+++ contracts/CatNFT.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CatNFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter = 0;
+
+    // 注意：OpenZeppelin 5.x 版本的 Ownable 构造函数需要传入初始 owner 地址
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+}
